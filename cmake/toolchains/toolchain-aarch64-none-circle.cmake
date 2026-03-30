5282892f055e8591772f273ce01460dd2e9b02ca
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2026-03-30 09:40:07 +0200

    Remove hardcoded compiler paths

--- cmake/toolchains/toolchain-aarch64-none-circle.cmake
+++ cmake/toolchains/toolchain-aarch64-none-circle.cmake
@@ -1,9 +1,9 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(CMAKE_C_COMPILER /home/stm/local/arm-gnu-toolchain-15.2.rel1-x86_64-aarch64-none-elf/bin/aarch64-none-elf-gcc)
-set(CMAKE_CXX_COMPILER /home/stm/local/arm-gnu-toolchain-15.2.rel1-x86_64-aarch64-none-elf/bin/aarch64-none-elf-g++)
-set(CMAKE_ASM_COMPILER /home/stm/local/arm-gnu-toolchain-15.2.rel1-x86_64-aarch64-none-elf/bin/aarch64-none-elf-gcc)
+set(CMAKE_C_COMPILER aarch64-none-elf-gcc)
+set(CMAKE_CXX_COMPILER aarch64-none-elf-g++)
+set(CMAKE_ASM_COMPILER aarch64-none-elf-gcc)
 
 # Optimization and CPU flags
 set(FLAGS "-mcpu=cortex-a53 -ffreestanding -mstrict-align")
