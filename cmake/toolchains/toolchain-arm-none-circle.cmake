b936160455bd83ebd19d4a9f34afe85b4af1133c
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2026-03-30 21:09:01 +0200

    Remove absolute pathnames from toolchain file

--- cmake/toolchains/toolchain-arm-none-circle.cmake
+++ cmake/toolchains/toolchain-arm-none-circle.cmake
@@ -1,9 +1,9 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(CMAKE_C_COMPILER /home/stm/local/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc)
-set(CMAKE_CXX_COMPILER /home/stm/local/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++)
-set(CMAKE_ASM_COMPILER /home/stm/local/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc)
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
 
 # Optimization and CPU flags
 set(FLAGS "-mcpu=cortex-a53 -ffreestanding -mstrict-align")
